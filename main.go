01f347a0ce1263bd37e665190d9c544953a43515
Henrique Lopes <riquellopes@gmail.com>
2017-12-24 17:37:19 -0200

    starting handlers

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -8,11 +10,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// ValidateHandler -
+func ValidateHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Not implemted")
+}
+
+// CreateTracker -
+func CreateTracker(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Not implemted")
+}
+
 func main() {
 
 	route := mux.NewRouter()
 
 	route.Handle("/", http.FileServer(http.Dir("./views/")))
 
-	http.ListenAndServe(":5000", handlers.LoggingHandler(os.Stdout, route))
+	// Api path
+	route.HandleFunc("/tracker/{id}", ValidateHandler).Methods("GET")
+	route.HandleFunc("/tracker", CreateTracker).Methods("POST")
+
+	if err := http.ListenAndServe(":5000", handlers.LoggingHandler(os.Stdout, route)); err != nil {
+		log.Fatal(err)
+	}
 }
