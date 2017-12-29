fa84f16acf97ed36d25df97e107c6c015b51295d
Henrique <everaldo.lopes@concrete.com.br>
2017-12-29 07:59:10 -0200

    creating a new structure

---
+++ backend/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
+	. "github.com/riquellopes/tracker-api/models"
+)
+
+var tracker = Tracker{}
+
+// ValidateTrackerHandler -
+func ValidateTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	valid, err := tracker.Exists(params["id"])
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if valid {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
+// CreateTrackerHandler -
+func CreateTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var tracker Tracker
+
+	statusCode := http.StatusOK
+	response, _ := json.Marshal(map[string]string{"result": "success"})
+
+	if err := json.NewDecoder(r.Body).Decode(&tracker); err != nil {
+		statusCode = http.StatusBadRequest
+		response, _ = json.Marshal(map[string]string{"result": "Invalid request payload"})
+
+		log.Println(err.Error())
+	}
+
+	if err := tracker.Add(); err != nil {
+		statusCode = http.StatusInternalServerError
+		response, _ = json.Marshal(map[string]string{"result": err.Error()})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(response)
+}
+
+// RegisterTrackerHandler -
+func RegisterTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	params := mux.Vars(r)
+	var tracker Tracker
+
+	statusCode := http.StatusOK
+	response, _ := json.Marshal(map[string]string{"result": "success"})
+
+	if err := json.NewDecoder(r.Body).Decode(&tracker); err != nil {
+		statusCode = http.StatusBadRequest
+		response, _ = json.Marshal(map[string]string{"result": "Invalid request payload"})
+
+		log.Println(err.Error())
+	}
+
+	if err := tracker.Register(params["id"]); err != nil {
+		statusCode = http.StatusInternalServerError
+		response, _ = json.Marshal(map[string]string{"result": err.Error()})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(response)
+}
+
+func init() {
+	var (
+		USERNAME = os.Getenv("USERNAME")
+		PASSWORD = os.Getenv("PASSWORD")
+		SERVER   = os.Getenv("SERVER")
+		DATABASE = os.Getenv("DATABASE")
+	)
+
+	log.Printf("MongoDB configuration: %s %s %s %s", USERNAME, PASSWORD, SERVER, DATABASE)
+
+	Connection(USERNAME, PASSWORD, SERVER, DATABASE)
+
+	log.Println("MongoDB connected.")
+}
+
+func main() {
+
+	route := mux.NewRouter()
+
+	route.Handle("/", http.FileServer(http.Dir("./views/")))
+
+	// Api path
+	route.HandleFunc("/tracker/{id}", ValidateTrackerHandler).Methods("GET")
+	route.HandleFunc("/tracker", CreateTrackerHandler).Methods("POST")
+	route.HandleFunc("/tracker/{id}", RegisterTrackerHandler).Methods("PUT")
+
+	if err := http.ListenAndServe(":5000", handlers.LoggingHandler(os.Stdout, route)); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ backend/models/tracker.go
@@ -0,0 +1,74 @@
+package models
+
+import (
+	"log"
+	"time"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var db *mgo.Database
+
+// Tracker -
+type Tracker struct {
+	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Name      string        `bson:"name" json:"name"`
+	Data      string        `bson:"data" json:"data"`
+	Opened    bool          `bson:"opened" json:"opened"`
+	CreateAt  time.Time     `bson:"create_at" json:"create_at"`
+	UpdatedAt time.Time     `bson:"update_at" json:"update_at"`
+}
+
+// Add -
+func (m *Tracker) Add() error {
+	m.CreateAt = time.Now()
+	m.ID = bson.NewObjectId()
+	m.Opened = false
+
+	err := db.C("tracker").Insert(&m)
+	return err
+}
+
+// Exists -
+func (m *Tracker) Exists(id string) (bool, error) {
+	count, error := db.C("tracker").Find(bson.M{"_id": bson.ObjectIdHex(id)}).Count()
+
+	if error != nil || count > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Register -
+func (m *Tracker) Register(id string) error {
+	m.ID = bson.ObjectIdHex(id)
+	m.UpdatedAt = time.Now()
+	m.Opened = true
+
+	selector := bson.M{"opened": false, "_id": bson.ObjectIdHex(id)}
+
+	if err := db.C("tracker").Update(selector, &m); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Connection -
+func Connection(username, password, server, database string) {
+	info := &mgo.DialInfo{
+		Addrs:    []string{server},
+		Timeout:  60 * time.Second,
+		Database: database,
+		Username: username,
+		Password: password,
+	}
+
+	session, err := mgo.DialWithInfo(info)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.SetMode(mgo.Monotonic, true)
+	db = session.DB(database)
+}
