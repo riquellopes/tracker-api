a2133ad9ef2406e8bc462b60de1428587d59102e
Henrique Lopes <riquellopes@gmail.com>
2018-01-09 22:18:32 -0200

    creating a empty row to render browser data

---
+++ frontend/src/components/RowData.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class RowData extends Component {
+
+    render(){
+        return (
+            <span></span>
+        )
+    }
+}
+
+
+export default Row
